26e91b355677ca3fa81029ecdf9db260cb2af895
Titouan B <nightbringer4ever@gmail.com>
2018-11-22 08:26:00 +0100

    fix

--- build/soon-colorpicker.js
+++ build/soon-colorpicker.js
@@ -1,5 +1,137 @@
 /*!
  * Built with http://stenciljs.com
- * 2018-11-22T07:21:38
+ * 2018-11-22T07:25:26
  */
-!function(e,t,r,o,n,s,i,c,l,a,u,p,d,h){for(u=e.SoonColorpicker=e.SoonColorpicker||{},(p=t.createElement("style")).innerHTML="soon-colorpicker{visibility:hidden}.hydrated{visibility:inherit}",p.setAttribute("data-styles",""),d=t.head.querySelector("meta[charset]"),t.head.insertBefore(p,d?d.nextSibling:t.head.firstChild),function(e,t,r){(e["s-apps"]=e["s-apps"]||[]).push("SoonColorpicker"),r.componentOnReady||(r.componentOnReady=function(){var t=this;function r(r){if(t.nodeName.indexOf("-")>0){for(var o=e["s-apps"],n=0,s=0;s<o.length;s++)if(e[o[s]].componentOnReady){if(e[o[s]].componentOnReady(t,r))return;n++}if(n<o.length)return void(e["s-cr"]=e["s-cr"]||[]).push([t,r])}r(null)}return e.Promise?new e.Promise(r):{then:r}})}(e,0,a),n=n||u.resourcesUrl,p=(d=t.querySelectorAll("script")).length-1;p>=0&&!(h=d[p]).src&&!h.hasAttribute("data-resources-url");p--);d=h.getAttribute("data-resources-url"),!n&&d&&(n=d),!n&&h.src&&(n=(d=h.src.split("/").slice(0,-1)).join("/")+(d.length?"/":"")+"soon-colorpicker/"),p=t.createElement("script"),function(e,t,r,o){return!(t.search.indexOf("core=esm")>0)&&(!(!(t.search.indexOf("core=es5")>0||"file:"===t.protocol)&&e.customElements&&e.customElements.define&&e.fetch&&e.CSS&&e.CSS.supports&&e.CSS.supports("color","var(--c)")&&"noModule"in r)||function(e){try{return new Function('import("")'),!1}catch(e){}return!0}())}(e,e.location,p)?p.src=n+"soon-colorpicker.vceajjzj.js":(p.src=n+"soon-colorpicker.kwijbony.js",p.setAttribute("type","module"),p.setAttribute("crossorigin",!0)),p.setAttribute("data-resources-url",n),p.setAttribute("data-namespace","soon-colorpicker"),t.head.appendChild(p)}(window,document,0,0,0,0,0,0,0,HTMLElement.prototype);
+(function(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCoreSsr, appCorePolyfilled, hydratedCssClass, components) {
+
+  function init(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCorePolyfilled, hydratedCssClass, cmpTags, HTMLElementPrototype, App, x, y, scriptElm) {
+    // create global namespace if it doesn't already exist
+    App = win[namespace] = win[namespace] || {};
+    if (cmpTags) {
+        // auto hide components until they been fully hydrated
+        // reusing the "x" and "i" variables from the args for funzies
+        x = doc.createElement('style');
+        x.innerHTML = cmpTags + '{visibility:hidden}.' + hydratedCssClass + '{visibility:inherit}';
+        x.setAttribute('data-styles', '');
+        y = doc.head.querySelector('meta[charset]');
+        doc.head.insertBefore(x, y ? y.nextSibling : doc.head.firstChild);
+    }
+    createComponentOnReadyPrototype(win, namespace, HTMLElementPrototype);
+    resourcesUrl = resourcesUrl || App.resourcesUrl;
+    // figure out the script element for this current script
+    y = doc.querySelectorAll('script');
+    for (x = y.length - 1; x >= 0; x--) {
+        scriptElm = y[x];
+        if (scriptElm.src || scriptElm.hasAttribute('data-resources-url')) {
+            break;
+        }
+    }
+    // get the resource path attribute on this script element
+    y = scriptElm.getAttribute('data-resources-url');
+    if (!resourcesUrl && y) {
+        // the script element has a data-resources-url attribute, always use that
+        resourcesUrl = y;
+    }
+    if (!resourcesUrl && scriptElm.src) {
+        // we don't have an exact resourcesUrl, so let's
+        // figure it out relative to this script's src and app's filesystem namespace
+        y = scriptElm.src.split('/').slice(0, -1);
+        resourcesUrl = (y.join('/')) + (y.length ? '/' : '') + fsNamespace + '/';
+    }
+    // request the core this browser needs
+    // test for native support of custom elements and fetch
+    // if either of those are not supported, then use the core w/ polyfills
+    // also check if the page was build with ssr or not
+    x = doc.createElement('script');
+    if (usePolyfills(win, win.location, x, 'import("")')) {
+        // requires the es5/polyfilled core
+        x.src = resourcesUrl + appCorePolyfilled;
+    }
+    else {
+        // let's do this!
+        x.src = resourcesUrl + appCore;
+        x.setAttribute('type', 'module');
+        x.setAttribute('crossorigin', true);
+    }
+    x.setAttribute('data-resources-url', resourcesUrl);
+    x.setAttribute('data-namespace', fsNamespace);
+    doc.head.appendChild(x);
+}
+function usePolyfills(win, location, scriptElm, dynamicImportTest) {
+    // fyi, dev mode has verbose if/return statements
+    // but it minifies to a nice 'lil one-liner ;)
+    if (location.search.indexOf('core=esm') > 0) {
+        // force esm build
+        return false;
+    }
+    if ((location.search.indexOf('core=es5') > 0) ||
+        (location.protocol === 'file:') ||
+        (!(win.customElements && win.customElements.define)) ||
+        (!win.fetch) ||
+        (!(win.CSS && win.CSS.supports && win.CSS.supports('color', 'var(--c)'))) ||
+        (!('noModule' in scriptElm))) {
+        // es5 build w/ polyfills
+        return true;
+    }
+    // final test to see if this browser support dynamic imports
+    return doesNotSupportsDynamicImports(dynamicImportTest);
+}
+function doesNotSupportsDynamicImports(dynamicImportTest) {
+    try {
+        new Function(dynamicImportTest);
+        return false;
+    }
+    catch (e) { }
+    return true;
+}
+function createComponentOnReadyPrototype(win, namespace, HTMLElementPrototype) {
+    (win['s-apps'] = win['s-apps'] || []).push(namespace);
+    if (!HTMLElementPrototype.componentOnReady) {
+        HTMLElementPrototype.componentOnReady = function componentOnReady() {
+            /*tslint:disable*/
+            var elm = this;
+            function executor(resolve) {
+                if (elm.nodeName.indexOf('-') > 0) {
+                    // window hasn't loaded yet and there's a
+                    // good chance this is a custom element
+                    var apps = win['s-apps'];
+                    var appsReady = 0;
+                    // loop through all the app namespaces
+                    for (var i = 0; i < apps.length; i++) {
+                        // see if this app has "componentOnReady" setup
+                        if (win[apps[i]].componentOnReady) {
+                            // this app's core has loaded call its "componentOnReady"
+                            if (win[apps[i]].componentOnReady(elm, resolve)) {
+                                // this component does belong to this app and would
+                                // have fired off the resolve fn
+                                // let's stop here, we're good
+                                return;
+                            }
+                            appsReady++;
+                        }
+                    }
+                    if (appsReady < apps.length) {
+                        // not all apps are ready yet
+                        // add it to the queue to be figured out when they are
+                        (win['s-cr'] = win['s-cr'] || []).push([elm, resolve]);
+                        return;
+                    }
+                }
+                // not a recognized app component
+                resolve(null);
+            }
+            // callback wasn't provided, let's return a promise
+            if (win.Promise) {
+                // use native/polyfilled promise
+                return new win.Promise(executor);
+            }
+            // promise may not have been polyfilled yet
+            return { then: executor };
+        };
+    }
+}
+
+
+  init(win, doc, namespace, fsNamespace, resourcesUrl, appCore, appCoreSsr, appCorePolyfilled, hydratedCssClass, components);
+
+  })(window, document, "SoonColorpicker","soon-colorpicker",0,"soon-colorpicker.core.js","soon-colorpicker.core.pf.js","hydrated","soon-colorpicker",HTMLElement.prototype);
---
+++ build/soon-colorpicker/soon-colorpicker.sc.entry.js
@@ -0,0 +1,189 @@
+/*! Built with http://stenciljs.com */
+const { h } = window.SoonColorpicker;
+
+class Colorpicker {
+    constructor() {
+        this.opened = false;
+        this.hasFocus = false;
+        /**
+         * Value as a string represent the color in hexadecimal with #.
+         */
+        this.value = null;
+    }
+    /**
+     * Method to manually close the color palette.
+     */
+    soonClose() {
+        this.onBlur();
+    }
+    /**
+     * Method to manually open the color palette.
+     */
+    soonOpen() {
+        this.opened = true;
+        this.onFocus();
+    }
+    valueChange() {
+        this._initColor();
+    }
+    imageChange(newImage) {
+        if (newImage) {
+            this._initCanvas();
+        }
+    }
+    handleEnter() {
+        this.soonOpen();
+    }
+    handleESC() {
+        this.soonClose();
+    }
+    componentDidLoad() {
+        this._initColor();
+        this._initCanvas();
+        // Bind host event focus and Blur for tabIndex feature
+        this.el.addEventListener('focus', () => {
+            this.onFocus();
+        });
+        this.el.addEventListener('blur', () => {
+            this.onBlur();
+        });
+    }
+    onBlur() {
+        this.hasFocus = false;
+        this.opened = false;
+        this.soonBlur.emit();
+    }
+    onFocus() {
+        this.hasFocus = true;
+        this.soonFocus.emit();
+    }
+    hasValue() {
+        return this.value !== null && this.value !== undefined && this.value !== '';
+    }
+    hostData() {
+        return {
+            role: 'colorpicker',
+            tabIndex: 0,
+            interactive: true,
+            class: {
+                'has-value': this.hasValue(),
+                'has-focus': this.hasFocus
+            }
+        };
+    }
+    render() {
+        return [h("div", { class: "color", onClick: () => (this.opened = true) }), h("canvas", { class: `selector ${this.opened ? 'opened' : ''}` })];
+    }
+    _initColor() {
+        const color = this.el.shadowRoot.querySelector('.color');
+        if (color) {
+            if (this.value) {
+                color.style.backgroundColor = this.value;
+            }
+            else {
+                // Reset backgroundColor
+                color.style.backgroundColor = '#fafbfd';
+            }
+        }
+    }
+    _setColor(color) {
+        this.value = color;
+        this.opened = false;
+        // emit value Change
+        const value = this.value;
+        this.soonChange.emit({ value });
+    }
+    _initCanvas() {
+        const canvas = this.el.shadowRoot.querySelector('.selector');
+        const color = this.el.shadowRoot.querySelector('.color');
+        canvas.width = 150;
+        canvas.height = 150;
+        let selectorImage = new Image();
+        selectorImage.crossOrigin = 'Anonymous';
+        selectorImage.onload = function () {
+            canvas.width = selectorImage.width;
+            canvas.height = selectorImage.height;
+            canvas.getContext('2d').drawImage(selectorImage, 0, 0, selectorImage.width, selectorImage.height);
+            // Center canvas on top of color
+            canvas.style.left = '-' + (selectorImage.width / 2 - color.clientWidth / 2) + 'px';
+        };
+        selectorImage.src = this.image;
+        canvas.onmousedown = (event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            var offset = event.target.getBoundingClientRect(), colorData = canvas.getContext('2d').getImageData(event.clientX - offset.left, event.clientY - offset.top, 1, 1).data;
+            const colorPicked = this._rgbToHex('rgb(' + colorData[0] + ',' + colorData[1] + ',' + colorData[2] + ')');
+            this._setColor(colorPicked);
+        };
+    }
+    /**
+     * Convert rgb to hex
+     */
+    _rgbToHex(rgb) {
+        var result = rgb.match(/\d+/g);
+        function hex(x) {
+            var digits = new Array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F');
+            return isNaN(x) ? '00' : digits[(x - (x % 16)) / 16] + digits[x % 16];
+        }
+        return '#' + hex(result[0]) + hex(result[1]) + hex(result[2]);
+    }
+    static get is() { return "soon-colorpicker"; }
+    static get encapsulation() { return "shadow"; }
+    static get properties() { return {
+        "el": {
+            "elementRef": true
+        },
+        "hasFocus": {
+            "state": true
+        },
+        "image": {
+            "type": String,
+            "attr": "image",
+            "watchCallbacks": ["imageChange"]
+        },
+        "opened": {
+            "state": true
+        },
+        "soonClose": {
+            "method": true
+        },
+        "soonOpen": {
+            "method": true
+        },
+        "value": {
+            "type": String,
+            "attr": "value",
+            "mutable": true,
+            "watchCallbacks": ["valueChange"]
+        }
+    }; }
+    static get events() { return [{
+            "name": "soonBlur",
+            "method": "soonBlur",
+            "bubbles": true,
+            "cancelable": true,
+            "composed": true
+        }, {
+            "name": "soonFocus",
+            "method": "soonFocus",
+            "bubbles": true,
+            "cancelable": true,
+            "composed": true
+        }, {
+            "name": "soonChange",
+            "method": "soonChange",
+            "bubbles": true,
+            "cancelable": true,
+            "composed": true
+        }]; }
+    static get listeners() { return [{
+            "name": "keydown.enter",
+            "method": "handleEnter"
+        }, {
+            "name": "keydown.escape",
+            "method": "handleESC"
+        }]; }
+    static get style() { return ".sc-soon-colorpicker-h {\n  \n  display: inline-block;\n  position: relative;\n  -webkit-box-sizing: border-box;\n  box-sizing: border-box;\n  width: var(--element-size, 32px);\n  height: var(--element-size, 32px);\n  border-radius: 50%;\n  padding: 2px;\n  background: #fff;\n  -webkit-box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.1);\n  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.1);\n  vertical-align: middle; }\n  .sc-soon-colorpicker-h   .color.sc-soon-colorpicker {\n    display: block;\n    width: calc(var(--element-size, 32px) - 4px);\n    height: calc(var(--element-size, 32px) - 4px);\n    border-radius: 50%;\n    background: #fafbfd;\n    cursor: pointer; }\n  .sc-soon-colorpicker-h   .selector.sc-soon-colorpicker {\n    position: absolute;\n    visibility: hidden;\n    opacity: 0;\n    -webkit-transform: scale(0);\n    transform: scale(0);\n    margin: auto;\n    top: 0;\n    bottom: 0;\n    cursor: crosshair;\n    -webkit-transition: all 0.2s ease-in-out;\n    transition: all 0.2s ease-in-out;\n    z-index: var(--element-zindex, 9999); }\n    .sc-soon-colorpicker-h   .selector.opened.sc-soon-colorpicker {\n      visibility: visible;\n      opacity: 1;\n      -webkit-transform: scale(1);\n      transform: scale(1); }\n\n.sc-soon-colorpicker-h:focus {\n  outline: none; }"; }
+}
+
+export { Colorpicker as SoonColorpicker };
